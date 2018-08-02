074573be3a17cfffc9859033b01f0486488c0796
Ryan Olejnik <ryan.olejnik@gmail.com>
2018-08-02 16:10:21 -0400

    Modify header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import NewPostForm from "./components/NewPostForm";
 import Posts from "./components/Posts";
@@ -12,8 +11,7 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Forum Ipsum</h1>
           </header>
           <div className="forumContainer">
             <NewPostForm />
